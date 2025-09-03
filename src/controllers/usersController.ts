814ec3048f4cbf9f26462cbc5db6d804c9d0702d
Marcus Vinicius <marcusv892@gmail.com>
2025-09-03 00:18:37 -0300

    feat: creat user controller and use router

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,5 @@
+class UserController {
+  async create(request, reply) {}
+}
+
+export default UserController;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,8 @@
 import type { FastifyInstance } from "fastify";
+import UserController from "../controllers/usersController.js";
+
+const usersController = new UserController();
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post("/");
+  app.post("/", usersController.create);
 }
