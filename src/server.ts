f733a74a42f535c1510a83636bdfa9f79a7b4f41
Marcus Vinicius <marcusv892@gmail.com>
2025-09-11 18:02:56 -0300

    feat: register router user

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import fastify from "fastify";
 import "dotenv/config";
+import { usersRoutes } from "./routes/users.js";
 
 const app = fastify();
+
+app.register(usersRoutes);
 
 app.listen({ port: Number(process.env.PORT) }).then(() => {
   console.log("Server is running");
