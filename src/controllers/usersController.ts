45935c0936b6282ade36d5ebb2603df866bd58c4
Marcus Vinicius <marcusv892@gmail.com>
2025-09-11 18:02:48 -0300

    feat: user controller method create

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,20 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 class UserController {
-  async create(request, reply) {}
+  async create(request: FastifyRequest, reply: FastifyReply) {
+    const { firstName, lastName, email, phoneNumber, password } = request.body;
+
+    const findUserWithEmail = prisma.user.findMany({
+      where: email,
+    });
+
+    if (findUserWithEmail) {
+      reply.code(400).send("Este endereço de email já está em uso");
+    }
+  }
 }
 
 export default UserController;
