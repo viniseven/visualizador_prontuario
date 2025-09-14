5bcd3dc14533834d3e44f88becb66bade63fd83d
Marcus Vinicius <marcusv892@gmail.com>
2025-09-13 23:57:06 -0300

    feat: create prisma data user in db

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,19 +1,35 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
+import { type FastifyReply, type FastifyRequest } from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { createUserSchema } from "../schemas/userSchema.js";
+import { hashPassword } from "../utils/bcrypt.js";
 
 const prisma = new PrismaClient();
 
 class UserController {
   async create(request: FastifyRequest, reply: FastifyReply) {
-    const { firstName, lastName, email, phoneNumber, password } = request.body;
+    const data = createUserSchema.parse(request.body);
 
-    const findUserWithEmail = prisma.user.findMany({
-      where: email,
+    const findUserWithEmail = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
     });
 
     if (findUserWithEmail) {
       reply.code(400).send("Este endereço de email já está em uso");
     }
+
+    const hashedPassword = await hashPassword(data.password);
+
+    await prisma.user.create({
+      data: {
+        firstName: data.firstName,
+        secondName: data.lastName,
+        email: data.email,
+        phoneNumber: data.phoneNumber,
+        password: hashedPassword,
+      },
+    });
   }
 }
 
