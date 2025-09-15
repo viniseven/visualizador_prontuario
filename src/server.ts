8718a73b6cd15218c8df5fc21006e9dfecbfd04f
Marcus Vinicius <marcusv892@gmail.com>
2025-09-15 17:24:31 -0300

    feat: added jwt plugin

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import fastify from "fastify";
-import "dotenv/config";
+import { env } from "./env";
 import { usersRoutes } from "./routes/users.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import fastifyJwt from "@fastify/jwt";
 
 const app = fastify();
 
 app.setErrorHandler(errorHandler);
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+});
+
 app.register(usersRoutes);
 
-app.listen({ port: Number(process.env.PORT) }).then(() => {
+app.listen({ port: Number(env.PORT) }).then(() => {
   console.log("Server is running");
 });
