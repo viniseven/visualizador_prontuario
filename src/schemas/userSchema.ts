057753f8ae08e81caa5381f060a3133add182590
Marcus Vinicius <marcusv892@gmail.com>
2025-09-15 12:08:12 -0300

    feat: validation zod error in createUserSchema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -2,11 +2,16 @@ import { z } from "zod";
 
 export const createUserSchema = z
   .object({
-    firstName: z.string().min(1),
-    lastName: z.string().min(1),
-    email: z.email(),
-    phoneNumber: z.string().max(11),
-    password: z.string().min(8),
+    firstName: z.string().nonempty({ error: "O nome é obrigatório" }),
+    lastName: z.string().nonempty({ error: "O sobrenome é obrigatório" }),
+    email: z
+      .email({ error: "Email inválido" })
+      .nonempty({ error: "O email é obrigatório" }),
+    phoneNumber: z.string().max(11, { error: "Número de telefone inválido" }),
+    password: z
+      .string()
+      .min(8, { error: "A senha deverá conter pelo menos 8 caracteres" })
+      .nonempty({ error: "A senha é obrigatória" }),
     confirmPassword: z.string().min(8),
   })
   .refine((data) => data.password === data.confirmPassword, {
