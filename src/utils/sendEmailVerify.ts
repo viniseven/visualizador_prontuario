7238bf70d72f1a56da2e40fdd9683b990da16526
Marcus Vinicius <marcusv892@gmail.com>
2025-09-15 23:50:12 -0300

    feat: handler send email register validation user

---
+++ src/utils/sendEmailVerify.ts
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+import { env } from "../env/index.js";
+import { type CreateUserSchema } from "../schemas/userSchema.js";
+
+type emailUserSchema = Pick<
+  CreateUserSchema,
+  "firstName" | "lastName" | "email" | "department" | "role"
+>;
+
+export async function handlerSendEmail(
+  user: emailUserSchema,
+  urlVerify: string
+) {
+  const transporter = nodemailer.createTransport({
+    service: env.SERVICE,
+    host: env.HOST,
+    port: env.PORT_SMTP,
+    secure: true,
+    auth: {
+      user: env.USER_MAIL,
+      pass: env.PASS_MAIL,
+    },
+  });
+
+  await transporter.sendMail({
+    from: "Visualizador de prontuário <env.USER_MAIL>",
+    to: user.email,
+    subject: "Validação de cadastro",
+    text: `<h1>Validação de cadastro de usuário</h1>
+          <h3>Dados do usuário</h3>
+          <p>Nome:${user.firstName} ${user.lastName}</p>
+          <p>Email:${user.email}</p>
+          <p>Setor:${user.department}</p>
+          <p>Função:${user.role}</p>
+          <link>${urlVerify}</link>
+    `,
+  });
+}
