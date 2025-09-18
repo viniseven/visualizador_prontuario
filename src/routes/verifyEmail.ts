b69c9bd32058014621cc54712361437a0fdd8468
Marcus Vinicius <marcusv892@gmail.com>
2025-09-17 22:34:31 -0300

    feat: added hook verify token

--- src/routes/verifyEmail.ts
+++ src/routes/verifyEmail.ts
@@ -1,8 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import VerifyEmailController from "../controllers/verifyEmailController.js";
+import { verifyTokenEmail } from "../hooks/verifyTokenEmail.js";
 
 const verifyEmailController = new VerifyEmailController();
 
 export async function verifyRoutes(app: FastifyInstance) {
-  app.get("/verify-email", verifyEmailController.create);
+  app.get(
+    "/verify-email",
+    { preHandler: [verifyTokenEmail] },
+    verifyEmailController.validate
+  );
 }
