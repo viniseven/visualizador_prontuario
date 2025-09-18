e3db27ccd5b1c903eb64b647ff4cd5b2be702ce3
Marcus Vinicius <marcusv892@gmail.com>
2025-09-17 22:34:51 -0300

    feat: hook verify token email params

---
+++ src/hooks/verifyTokenEmail.ts
@@ -0,0 +1,21 @@
+import { type FastifyReply, type FastifyRequest } from "fastify";
+import { z } from "zod";
+
+const verifyTokenSchema = z.object({
+  token: z.string().nonempty({ error: "O link é inválido" }),
+});
+
+export async function verifyTokenEmail(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const { token } = verifyTokenSchema.parse(request.query);
+
+    const decodedToken = reply.server.jwt.verify(token);
+
+    request.user = decodedToken;
+  } catch (error) {
+    return reply.code(400).send({ message: "Link inválido ou expirado" });
+  }
+}
