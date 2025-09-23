a80ab3c43205c73af90a8392747a5543f60ecc2f
Marcus Vinicius <marcusv892@gmail.com>
2025-09-23 16:40:10 -0300

    feat: authentication and route signin

---
+++ src/controllers/authenticationController.ts
@@ -0,0 +1,35 @@
+import { type FastifyReply, type FastifyRequest } from "fastify";
+import { loginSchema } from "../schemas/loginSchema.js";
+import { PrismaClient } from "@prisma/client";
+import { generateToken } from "../utils/generateToken.js";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+class AuthenticationController {
+  async create(request: FastifyRequest, reply: FastifyReply) {
+    const data = loginSchema.parse(request.body);
+
+    const findUser = await prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!findUser) {
+      return reply.status(400).send({ error: "Email ou senha inválido" });
+    }
+
+    const matchedPassword = await bcrypt.compare(
+      data.password,
+      findUser?.password
+    );
+
+    if (!matchedPassword) {
+      return reply.status(400).send({ error: "Email ou senha inválido" });
+    }
+
+    const token = await generateToken(findUser.id, reply);
+    return reply.status(200).send({ findUser, token });
+  }
+}
+
+export default AuthenticationController;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import UserController from "../controllers/usersController.js";
+import AuthenticationController from "../controllers/authenticationController.js";
 
 const usersController = new UserController();
+const authenticationController = new AuthenticationController();
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/signup", usersController.create);
+  app.post("/signin", authenticationController.create);
 }
