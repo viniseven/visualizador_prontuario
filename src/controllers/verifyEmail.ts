5aa24053e2eff84c59a80a471a954514bc2bcc20
Marcus Vinicius <marcusv892@gmail.com>
2025-09-25 17:02:09 -0300

    refactor: name controller class

---
+++ src/controllers/verifyEmail.ts
@@ -0,0 +1,33 @@
+import { type FastifyReply, type FastifyRequest } from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class VerifyEmail {
+  async validate(request: FastifyRequest, reply: FastifyReply) {
+    const { sub: userId } = request.user;
+
+    const findUserWithId = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!findUserWithId) {
+      return reply.code(404).send({ message: "Usuário não encontrado" });
+    }
+
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        isVerified: true,
+      },
+    });
+
+    return reply.code(200).send({ message: "Usuário verificado com sucesso" });
+  }
+}
+
+export default VerifyEmail;
